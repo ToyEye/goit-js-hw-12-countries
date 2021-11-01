7ad8e8913c519caf0a0eb4fb5e4f90ba5f62010d
Korotenko Alex <alexey.korotenko90@gmail.com>
2021-11-01 11:03:24 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import './sass/main.scss';
+import debounce from 'lodash';
+import { error, Stack } from '@pnotify/core';
+import '@pnotify/core/dist/BrightTheme.css';
+import fetchCountries from "./js/fetchCountries.js"
+import cardList from "./partials/countriesList.hbs";
+
+const input = document.querySelector('#input');
+const container = document.querySelector('.container-country');
+
+input.addEventListener('input', debounce(writeInput, 500));
+
+function writeInput() {
+    fetchCountries(input.value).then(data => {
+        if (data.lengtrh > 10) {
+            console.log('Error');
+        } else if (data.length > 2 && data.length < 10) {
+            markUpContries(cardList(data));
+        } else {
+            console.log('Wow');
+        }
+        return;
+    })
+
+    function markUpContries(markup) {
+        container.innerHTML = '';
+        if (markup) container.insertAdjacentHTML('afterbegin', markup);
+        return;
+    }
+}
