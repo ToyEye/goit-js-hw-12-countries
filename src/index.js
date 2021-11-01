33fd2b2726431a18f49e99c0a240e14261ce9058
Korotenko Alex <alexey.korotenko90@gmail.com>
2021-11-01 15:35:36 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import './sass/main.scss';
-import debounce from 'lodash';
-import { error, Stack } from '@pnotify/core';
+import debounce from 'lodash.debounce';
+import { error } from '@pnotify/core';
 import '@pnotify/core/dist/BrightTheme.css';
 import fetchCountries from "./js/fetchCountries.js"
 import cardList from "./partials/countriesList.hbs";
+import countryCard from "./partials/countryCard.hbs";
 
 const input = document.querySelector('#input');
 const container = document.querySelector('.container-country');
@@ -11,13 +12,20 @@ const container = document.querySelector('.container-country');
 input.addEventListener('input', debounce(writeInput, 500));
 
 function writeInput() {
+    if (!input.value) {
+        return markUpContries(0);
+    }
     fetchCountries(input.value).then(data => {
-        if (data.lengtrh > 10) {
-            console.log('Error');
-        } else if (data.length > 2 && data.length < 10) {
+        if (!data.length) {
+            markUpContries(0);
+            return errMessage('Введите название страны')
+        }
+        if (data.length > 10) {
+            errMessage('Пожалуйста, введите более конкретный запрос!');
+        } else if (data.length >= 2 && data.length <= 10) {
             markUpContries(cardList(data));
         } else {
-            console.log('Wow');
+            markUpContries(countryCard(data[0]));
         }
         return;
     })
@@ -28,3 +36,17 @@ function writeInput() {
         return;
     }
 }
+
+
+function errMessage(message) {
+    return error({
+      text: message,
+      delay: 2000,
+      closer: false,
+      title: 'ОШИБКА!',
+      icon: false,
+      width: '250px',
+      sticker: false,
+      addClass: 'error-box',
+    });
+  }
